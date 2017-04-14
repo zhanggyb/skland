de60df55205c91b69d3a0a78a5ddac6e5283a3b4
Freeman Zhang <zhanggyb@gmail.com>
2017-04-14 18:07:00 +0800

    Initial trace support.

---
+++ test/unit/core-trace/test.cpp
@@ -0,0 +1,50 @@
+//
+// Created by zhanggyb on 16-9-19.
+//
+
+#include "test.hpp"
+
+#include <skland/core/trace.hpp>
+
+using namespace skland;
+
+Test::Test()
+    : testing::Test() {
+}
+
+Test::~Test() {
+
+}
+
+class TestCase {
+
+ public:
+
+  TestCase() {}
+
+  ~TestCase() {}
+
+  void TestDepth1() {
+    Trace trace(__PRETTY_FUNCTION__, "%s", "Test depth 1");
+    TestDepth2();
+  }
+
+  void TestDepth2() {
+    Trace trace(__PRETTY_FUNCTION__, "%s", "Test depth 2");
+    TestDepth3();
+  }
+
+  void TestDepth3() {
+    Trace trace(__PRETTY_FUNCTION__, "%s", "Test depth 3");
+  }
+
+};
+
+TEST_F(Test, constructor_1) {
+  TestCase t;
+  t.TestDepth1();
+
+  TRACE(__PRETTY_FUNCTION__, "%s", "Another message");
+
+  ASSERT_TRUE(true);
+}
