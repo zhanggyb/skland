f3ce2eac4e4904fde0a72ff6d3560a8c18566456
Freeman Zhang <zhanggyb@gmail.com>
2017-08-29 11:33:37 +0800

    Initial shared_ptr/week_ptr code.

--- include/skland/core/memory.hpp
+++ include/skland/core/memory.hpp
@@ -18,9 +18,84 @@
 #define SKLAND_CORE_MEMORY_HPP_
 
 #include <memory>
+#include <atomic>
 
 namespace skland {
 namespace core {
+
+/**
+ * @ingroup core
+ * @brief The base class for objects that may be shared by multiple objects.
+ * @tparam T The type of reference count, must be int, int32_t, long, size_t etc.
+ */
+template<typename T = int32_t>
+class SPCountedBase {
+
+  template<typename R> friend
+  class SharedPtr;
+
+  template<typename R> friend
+  class WeekPtr;
+
+ public:
+
+  SPCountedBase(const SPCountedBase &) = delete;
+  SPCountedBase(SPCountedBase &&) = delete;
+  SPCountedBase &operator=(const SPCountedBase &) = delete;
+  SPCountedBase &operator=(SPCountedBase &&) = delete;
+
+  SPCountedBase()
+      : reference_count_(1) {}
+
+  virtual ~SPCountedBase() = default;
+
+  T reference_count() const {
+    return reference_count_.load(std::memory_order_relaxed);
+  }
+
+  bool unique() const {
+    return 1 == reference_count();
+  }
+
+ private:
+
+  std::atomic<T> reference_count_;
+
+};
+
+template<typename T>
+class SharedPtr {
+
+ public:
+
+  SharedPtr() : ptr_(nullptr) {}
+
+  explicit SharedPtr(T *obj) : ptr_(obj) {
+    // TODO: fetch and add refcount
+  }
+
+ private:
+
+  T *ptr_;
+
+};
+
+template<typename T>
+class WeekPtr {
+
+ public:
+
+  WeekPtr() = default;
+
+  WeekPtr(const WeekPtr &other);
+
+  WeekPtr(const SharedPtr<T> &shared_ptr);
+
+ private:
+
+  SharedPtr<T> shared_ptr_;
+
+};
 
 /**
  * @ingroup core
